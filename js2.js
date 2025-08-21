07da94a691c12a3d1b3ee30597439bf7f19ce4bc
Parllon <parllon@gmail.com>
2025-08-21 11:38:11 -0300

    Execicio 2

---
+++ js2.js
@@ -0,0 +1,10 @@
+function exibirValorA(){
+    alert("O valor da variável a é "+a+" e seu tipo é"+typeof a);
+}
+function alterarValorA(){}
+
+function exibirValorB(){}
+function alterarValorB(){}
+
+function exibirValorC(){}
+function alterarValorC(){}
