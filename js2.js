d2b3ab391b91bedbea15e19adaf3a606fcdae46f
Parllon <parllon@gmail.com>
2025-08-21 16:41:14 -0300

    Execicio js2

--- js2.js
+++ js2.js
@@ -1,10 +1,23 @@
-function exibirValorA(){
-    alert("O valor da variável a é "+a+" e seu tipo é"+typeof a);
+function exibirValorA() {
+    alert("O valor da variável a é " + a + " e seu tipo é " + typeof a);
 }
-function alterarValorA(){}
+function alterarValorA() {
+    var a = "João"; //Redeclaração de escopo de função
+    alert("Valor da variável a foi alterado localmente para " + a);
+}
 
-function exibirValorB(){}
-function alterarValorB(){}
+function exibirValorB() {
+    alert("O valor da variável b é " + b + " e seu tipo é " + typeof b);
+}
+function alterarValorB() {
+    b = 30; //Alteração do valor no escopo global
+    alert("Valor da variável b foi alterado globalmente para " + b);
+}
 
-function exibirValorC(){}
-function alterarValorC(){}
+function exibirValorC() {
+    alert("O valor da variável c é " + c + " e seu tipo é " + typeof c);
+}
+function alterarValorC() {
+    const c = 10; //Redeclaração no escopo de função
+    alert("Valor da variável c foi alterado localmente para " + c);
+}
